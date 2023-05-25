84ffdf9b76a5aca7e3cd4949423053890975eef4
Banoofar <banoofar.shiran@gmail.com>
2023-05-25 17:27:06 +0430

    updated js and html

--- src/script.js
+++ src/script.js
@@ -1,27 +1,26 @@
 function showCityTemperature(response) {
-  let temp = Math.round(response.data.main.temp);
-  let wind = Math.round(response.data.wind.speed);
+  let h1 = document.querySelector("#h1");
+  let textCity = document.querySelector("#text-city");
   let cityTemp = document.querySelector("#temperature");
-  cityTemp.innerHTML = `${temp}`;
-  let cityHumid = document.querySelector("#wind");
-  cityHumid.innerHTML = `${wind}`;
+  let cityWind = document.querySelector("#wind");
   let cityDescription = document.querySelector("#weather-mood");
-  cityDescription.innerHTML = response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
 
-  let iconElement = document.querySelector("#icon");
+  h1.innerHTML = response.data.name;
+  textCity.innerHTML = response.data.name;
+  cityTemp.innerHTML = Math.round(response.data.main.temp);
+  cityWind.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  cityDescription.innerHTML = response.data.weather[0].description;
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
 function findCity(city) {
-  let h1 = document.querySelector("#h1");
-  h1.innerHTML = `${city}`;
-  let textCity = document.querySelector("#text-city");
-  textCity.innerHTML = `${city}`;
   let key = "c9b1d26ba353d1f56882373209bf2852";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
@@ -32,31 +31,6 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-name");
   findCity(cityInputElement.value);
-}
-
-function displayCurrent(response) {
-  let h1 = document.querySelector("#h1");
-  h1.innerHTML = "Current Location";
-  let currentTemp = document.querySelector("#current-temp");
-  currentTemp.innerHTML = `${response.data.main.temp}°C|F°`;
-  let currentHumid = document.querySelector("#current-hmd");
-  currentHumid.innerHTML = `${response.data.wind.speed}km/h`;
-  let currentDescription = document.querySelector("#weather-mood");
-  currentDescription.innerHTML = response.data.weather[0].description;
-  let currentLoc = document.querySelector("#text-city");
-  currentLoc.innerHTML = "your current location";
-  celsiusTemperature = response.data.main.temp;
-}
-
-function showPosition(position) {
-  console.log(position);
-
-  let key2 = "c9b1d26ba353d1f56882373209bf2852";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key2}&units=metric`;
-
-  axios.get(url).then(displayCurrent);
 }
 
 function displayFahrenheitTemperature(event) {
@@ -77,22 +51,46 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+function displayCurrent(response) {
+  let h1 = document.querySelector("#h1");
+  h1.innerHTML = "Current Location";
+  let currentTemp = document.querySelector("#temperature");
+  currentTemp.innerHTML = Math.round(response.data.main.temp);
+  let currentHumid = document.querySelector("#wind");
+  currentHumid.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  let currentDescription = document.querySelector("#weather-mood");
+  currentDescription.innerHTML = response.data.weather[0].description;
+  let currentLoc = document.querySelector("#text-city");
+  currentLoc.innerHTML = "your current location";
+}
+
+function showPosition(position) {
+  console.log(position);
+
+  let key2 = "c9b1d26ba353d1f56882373209bf2852";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key2}&units=metric`;
+
+  axios.get(url).then(displayCurrent);
+}
+
 function showCurrentLocation() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
 let celsiusTemperature = null;
 
-let currentLocation = document.querySelector("#current-location");
-currentLocation.addEventListener("click", showCurrentLocation);
-
-let form = document.querySelector("#search");
-form.addEventListener("submit", findCity);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", showCurrentLocation);
 
 findCity("Tehran");
